1514c6630d79de94da9b6ca28879d3beaf7f9083
RAza ALy <007razajutt@gmail.com>
2021-07-14 11:18:39 +0500

    add drag drop 👍

--- src/App.js
+++ src/App.js
@@ -1,12 +1,106 @@
- import React from 'react'
- 
- const App = () => {
-   return (
-     <div>
-       <h1>task</h1>
-     </div>
-   )
- }
- 
- export default App;
- 
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  const dragStart = (e) => {
+    const target = e.target;
+    e.dataTransfer.setData("card_id", target.id);
+    setTimeout(() => {
+      target.style.display = "none";
+    }, 0);
+  };
+  const dragOver = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  const drop = (e) => {
+    e.preventDefault();
+    const card_id = e.dataTransfer.getData("card_id");
+    const card = document.getElementById(card_id);
+    card.style.display = "block";
+    e.target.appendChild(card);
+  };
+  const onDragOver = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="app">
+      <div className="main-container" onDrop={drop} onDragOver={onDragOver}>
+        <div
+          className="box"
+          id="box1"
+          onDrop={drop}
+          onDragOver={dragOver}
+          draggable="true"
+          onDragStart={dragStart}
+        >
+          <div
+            className="card"
+            id="card1"
+            draggable="true"
+            onDragStart={dragStart}
+            onDragOver={dragOver}
+          >
+            <h1>Card 1</h1>
+          </div>
+          <div
+            className="card"
+            id="card2"
+            draggable="true"
+            onDragStart={dragStart}
+            onDragOver={dragOver}
+          >
+            <h1>Card 2</h1>
+          </div>
+          <div
+            className="card"
+            id="card3"
+            draggable="true"
+            onDragStart={dragStart}
+            onDragOver={dragOver}
+          >
+            <h1>Card 3</h1>
+          </div>
+        </div>
+        <div
+          className="box"
+          id="box2"
+          onDrop={drop}
+          onDragOver={dragOver}
+          draggable="true"
+          onDragStart={dragStart}
+        >
+          <div
+            className="card"
+            id="card4"
+            draggable="true"
+            onDragStart={dragStart}
+            onDragOver={dragOver}
+          >
+            <h1>Card 4</h1>
+          </div>
+        </div>
+        <div
+          className="box"
+          id="box3"
+          onDrop={drop}
+          onDragOver={dragOver}
+          draggable="true"
+          onDragStart={dragStart}
+        >
+          <div
+            className="card"
+            id="card5"
+            draggable="true"
+            onDragStart={dragStart}
+            onDragOver={dragOver}
+          >
+            <h1>Card 5</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
